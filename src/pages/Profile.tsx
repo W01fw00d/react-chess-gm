ba399efdf4022304e51247f0a5de6ecab21fef40
Gabriel Romay Machado <romay.gabriel@gmail.com>
2024-05-05 14:27:31 +0200

    Step 2: basic display of all the profile information

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,18 +2,105 @@ import { Link, useParams } from "react-router-dom";
 
 import { useQuery } from "@tanstack/react-query";
 
+enum Status {
+  closed,
+  "closed:fair_play_violations",
+  basic,
+  premium,
+  mod,
+  staff,
+}
+
+// Based on https://www.chess.com/news/view/published-data-api#pubapi-endpoint-games-archive
+type Response = {
+  "@id": string; // Do not display, doesn't seem like an url a end-user should see
+  url: string;
+  username: string;
+  player_id: number;
+  title?: string; // Optional
+  status: Status;
+  name?: string; // Optional
+  avatar?: string; // Optional
+  location?: string; // Optional
+  country: string;
+  joined: number; // timestamp
+  last_online: number; // timestamp
+  followers: number;
+  is_streamer: boolean;
+  twitch_url: string;
+  fide: number;
+
+  // Docs doesn't seem to be updated, they don't include some extra fields that sometimes are on the response:
+  league: string;
+  streaming_platforms: string[];
+  verified: boolean;
+};
+
+const ExternalLink = ({ url }: { url: string }) => <a href={url}>{url}</a>;
+
 const Profile = () => {
-  /* const GET_ALL_GRANDMASTERS = "titled/GM";
-
-  const query = useQuery<{ players: string[] }>({
-    queryKey: [GET_ALL_GRANDMASTERS],
-  }); */
-
   const { username } = useParams();
+
+  const query = useQuery<Response>({
+    queryKey: [`player/${username}`],
+  });
+
+  console.log({ data: query.data });
+
+  const { data } = query;
+
+  const joinedInDate = new Date(0);
+  joinedInDate.setUTCSeconds(data?.joined || 0);
+
+  const lastOnlineDate = new Date(0);
+  lastOnlineDate.setUTCSeconds(data?.last_online || 0);
 
   return (
     <div>
-      <div>{username}</div>
+      {data && (
+        <div>
+          <img src={data.avatar} alt={`${data.username}'s Avatar`} />
+          <div>Username: {data.username}</div>
+          <div>Full name: {data.name}</div>
+          <div>
+            Url: <ExternalLink url={data.url} />
+          </div>
+          <br />
+          <div>Location: {data.location}</div>
+          <div>
+            Country Url: <ExternalLink url={data.country} />
+          </div>
+          <br />
+          <div>Joined in: {joinedInDate.toLocaleDateString()}</div>
+          <div>Last online: {lastOnlineDate.toLocaleDateString()}</div>
+          <br />
+          <div>Title: {data.title}</div>
+          <div>League: {data.league}</div>
+          <div>FIDE rating: {data.fide}</div>
+          <br />
+          <div>Followers: {data.followers}</div>
+          <div>Is streamer? {data.is_streamer ? "Yes" : "No"} </div>
+          {data.streaming_platforms?.length > 0 && (
+            <div>
+              Streaming Platforms:
+              {data.streaming_platforms.map((platform) => (
+                <div>{platform}</div>
+              ))}
+            </div>
+          )}
+          {data.twitch_url && (
+            <div>
+              Twitch Url: <ExternalLink url={data.twitch_url} />
+            </div>
+          )}
+          <br />
+          <div>ID: {data.player_id}</div>
+          <div>Status: {data.status}</div>
+          <div>Is verified? {data.verified ? "Yes" : "No"} </div>
+        </div>
+      )}
+
+      <br />
       <br />
       <Link to={{ pathname: "/" }}>Go back to list</Link>
     </div>
